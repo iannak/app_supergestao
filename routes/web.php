29ab2e32e763137c9c8e2edfd2d5a549fa02811c
franzannakarolina <annakarolinafranz91@outlook.com>
2023-02-14 00:32:29 -0300

    Createad routes login and clients and products

--- routes/web.php
+++ routes/web.php
@@ -19,30 +19,10 @@ Route::get('/', function () {
 */
 
 Route::get('/', 'PrincipalController@principal');
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
 Route::get('/contato', 'ContatoController@contato');
-// nome, categoria, assunto, mensagem
 
-// Route::get(
-//     '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
-//     function(
-//         string $nome = 'Desconhecido',
-//         string $categoria = 'Informação',
-//         string $assunto = 'Contato',
-//         string $mensagem = 'mensagem não informada'
-//     ) {
-//         echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
-//     }
-// );
-
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function(
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1 // 1 - 'Informação'
-    ) {
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+Route::get('/login', function(){return 'Login';});
+Route::get('/clientes', function(){return 'Clientes';});
+Route::get('/fornecedores', function(){return 'Fornecedores';});
+Route::get('/produtos', function(){return 'produtos';});
