9bd163da42d8addcc274f0bd741e6db3283b5ec0
franzannakarolina <annakarolinafranz91@outlook.com>
2023-02-22 00:59:20 -0300

    agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,10 @@ Route::get('/', function () {
 Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
-
 Route::get('/login', function(){return 'Login';});
-Route::get('/clientes', function(){return 'Clientes';});
-Route::get('/fornecedores', function(){return 'Fornecedores';});
-Route::get('/produtos', function(){return 'produtos';});
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';});
+    Route::get('/fornecedores', function(){return 'Fornecedores';});
+    Route::get('/produtos', function(){return 'produtos';});
+});
