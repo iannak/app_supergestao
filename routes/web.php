9f4512065497e8e842b0f17182413fcffa6771b9
franzannakarolina <annakarolinafranz91@outlook.com>
2023-03-09 10:02:55 -0300

    blade @if/else and tests

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,12 @@ Route::get('/login', function(){return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
     Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
