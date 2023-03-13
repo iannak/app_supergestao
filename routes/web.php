4df9ecdd199f8f7ccad6918e11a00cd4b8f0f771
franzannakarolina <annakarolinafranz91@outlook.com>
2023-03-13 01:41:44 -0300

    feat: method empty

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
-Route::prefix('/app')->group(function(){
+Route::prefix('/app')->group(function() {
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
     Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
