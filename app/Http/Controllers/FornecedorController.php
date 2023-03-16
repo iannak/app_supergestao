85ccd128b4c137b359196de557b169005caef884
franzannakarolina <annakarolinafranz91@outlook.com>
2023-03-15 23:29:04 -0300

    ternary condicional operator value default

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -11,20 +11,14 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00'
+                'cnpj' => '0'
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
-                'status' => 'S'
+                'status' => 'S',
+                'cnpj' => null
             ]
         ];
-
-        /*
-        condicao ? se verdade : se falso;
-        condicao ? se verdade : (condicao ? se verdade : se falso);
-        */
-        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
-        echo $msg;
 
         return view('app.fornecedor.index', compact('fornecedores'));
     }
