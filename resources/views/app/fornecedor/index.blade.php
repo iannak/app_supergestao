9d3c1f3ca5d8011da7f1162aca488ff969c41b47
iannakfranz <annakarolinafranz91@outlook.com>
2023-06-01 22:33:01 -0300

    blade @foreach

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -15,14 +15,16 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
-    <!--
-        $variável testada não estiver definida (isset)
-        ou
-        $variável testada possui o valor null
-    -->
+
+    @foreach($fornecedores as $indice => $fornecedor)
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <hr>
+    @endforeach
 @endisset
+
