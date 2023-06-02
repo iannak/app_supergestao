a9b560adea8e9dd4099fc4a9d493e753ac0125ce
iannakfranz <annakarolinafranz91@outlook.com>
2023-06-01 22:27:32 -0300

    blade @whilte

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -15,14 +15,18 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
-    <!--
-        $variável testada não estiver definida (isset)
-        ou
-        $variável testada possui o valor null
-    -->
+
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+        <hr>
+        @php $i++ @endphp
+    @endwhile
 @endisset
+
